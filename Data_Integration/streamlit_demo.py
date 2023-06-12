152bfbfce17f0f80566587933b518e9d891d1b3e
Jayraj Parmar <jayrajparmar009@gmail.com>
2023-06-11 22:26:40 -0400

    Changed the way to read data from AWS S3 Bucket

--- Data_Integration/streamlit_demo.py
+++ Data_Integration/streamlit_demo.py
@@ -7,8 +7,21 @@ import plotly.graph_objects as go
 import streamlit as st
 
 # Reading data file directly from Amazon AWS S3
-final_df = pd.read_csv("s3://healthmarkers/final_df.csv",
-                   storage_options={"anon": False})
+# final_df = pd.read_csv("s3://healthmarkers/final_df.csv",
+#                    storage_options={"anon": False})
+
+
+from io import StringIO
+
+s3 = s3fs.S3FileSystem(anon=True)
+s3.ls('healthmarkers')
+with s3.open('healthmarkers/final_df.csv', 'rb') as f:
+    byte_s = f.read()
+
+s=str(byte_s,'utf-8')
+
+data = StringIO(s)
+final_df=pd.read_csv(data)
 
 
 
