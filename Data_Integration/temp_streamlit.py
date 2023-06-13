092695a50d01824c09c77b82c1de01bb7ee5b544
Jayraj Parmar <38021099+jayrajparmar009@users.noreply.github.com>
2023-06-12 22:50:32 -0400

    Update temp_streamlit.py
    updated bucket names

--- Data_Integration/temp_streamlit.py
+++ Data_Integration/temp_streamlit.py
@@ -4,5 +4,5 @@ from st_files_connection import FilesConnection
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.experimental_connection('s3', type=FilesConnection)
-df = conn.read("testbucket-jrieke/myfile.csv", input_format="csv", ttl=600)
+df = conn.read("healthmarkers/final_df.csv", input_format="csv", ttl=600)
 print(df.head())
