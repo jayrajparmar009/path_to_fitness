8c588d4511202e5b205ae7a67a5e9d7f4f62c5ca
Jayraj Parmar <38021099+jayrajparmar009@users.noreply.github.com>
2023-06-12 22:53:14 -0400

    Update streamlit_demo.py
    Page_config placement change

--- Data_Integration/streamlit_demo.py
+++ Data_Integration/streamlit_demo.py
@@ -16,23 +16,11 @@ from st_files_connection import FilesConnection
 
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
+st.set_page_config(page_title='Personal Health Marker Report',  layout='wide', page_icon='🏋️‍♂️')
 conn = st.experimental_connection('s3', type=FilesConnection)
 final_df = conn.read("healthmarkers/final_df.csv", input_format="csv", ttl=600)
 
 
-# s3 = s3fs.S3FileSystem(anon=True)
-# s3.ls('healthmarkers')
-# with s3.open('healthmarkers/final_df.csv', 'rb') as f:
-#     byte_s = f.read()
-
-# s=str(byte_s,'utf-8')
-
-# data = StringIO(s)
-# final_df=pd.read_csv(data)
-
-
-
-st.set_page_config(page_title='Personal Health Marker Report',  layout='wide', page_icon='🏋️‍♂️')
 
 # Filtering file for desired date range
 df = final_df[(~final_df.Weightkg.isnull()) & (final_df.date_range > '2021-08-01')]
